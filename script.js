bfab321b4b51a27ed65f92a1654ecc60804bc955
Virginia Escolar <virgi.escolar.ruiz@gmail.com>
2025-06-26 23:36:18 +0200

    Añadido condicionales

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ let lista = [1, 2, 3];         // Array
 let objeto = { clave: "valor" }; // Object
 let nada = null;              // Null
 let indefinido;               // Undefined
+
+
+// CONDICIONALES
+
+let edad = 18;
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+// Operador ternario
+let mensaje = edad >= 18 ? "Mayor de edad" : "Menor de edad";
+console.log(mensaje);
