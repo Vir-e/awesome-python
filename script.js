d3d03e4c49ad055f7d48db81d847c7b427b0ef18
Virginia Escolar <virgi.escolar.ruiz@gmail.com>
2025-06-26 23:35:17 +0200

    Añadidos tipos de datos

---
+++ script.js
@@ -0,0 +1,9 @@
+// TIPOS DE DATOS EN JAVASCRIPT
+
+let numero = 10;               // Number
+let texto = "Hola";            // String
+let booleano = true;           // Boolean
+let lista = [1, 2, 3];         // Array
+let objeto = { clave: "valor" }; // Object
+let nada = null;              // Null
+let indefinido;               // Undefined
